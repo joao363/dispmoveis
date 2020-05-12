35185d8914f9e166c579ca380f9b2915d3dfd58b
mariliamatosbiz <mariliamts98@gmail.com>
2020-05-11 22:22:38 -0300

    Dicionario

---
+++ app/src/main/java/com/example/dicionario/DictDbContract.java
@@ -0,0 +1,14 @@
+package com.example.dicionario;
+
+import android.provider.BaseColumns;
+
+public final class DictDbContract {
+    private DictDbContract() {}
+
+    public static class DictEntry implements BaseColumns{
+        public static final String NOME_TABELA = "dicionario";
+        public static final String COLUNA_PALAVRA = "palavra";
+        public static final String COLUNA_SINONIMO = "sinonimo";
+    }
+
+}
